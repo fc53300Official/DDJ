8b6f137965ac007edd1fb9ff2964ff6af51b0d6d
Gonçalo Rocha <65404659+fc51202@users.noreply.github.com>
2022-01-25 16:00:51 +0000

    Laser Fix with Timer (just one)
    Faço depois os restantes.

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    public float timeTilDestroy;
+
+    void Update()
+    {
+        Destroy(gameObject, timeTilDestroy);
+    }
+}
